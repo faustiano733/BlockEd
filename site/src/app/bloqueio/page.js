5a3161a647772eb89e1108b7b6210c98b5f00c43
ceutron <ceutron7@gmail.com>
2024-12-06 13:11:28 +0100

    Actualização da página de bloqueio

--- site/src/app/bloqueio/page.js
+++ site/src/app/bloqueio/page.js
@@ -7,41 +7,139 @@ import Header from "@components/Header.js";
 
 
 export default function Bloqueios(){
-  const [menuActual, setMenuActual] = useState("outros");
+  const [menuActual, setMenuActual] = useState("aplicativos");
+  const [subMenu, setSubMenu] = useState(null);
   const [diretorios, setDiretorios] = useState("");
+  const [sites, setSites] = useState([{domain: "facebook.com"}, {domain: "tiktok.com"}]);
+  const [apps, setApps] = useState([{name: "Facebook"}, {name: "TikTok"}]);
+  
+  const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
   function BloqueioItem(props){
     return(
-      <div className={props.menu == menuActual ? "bloqueioItem bloqueioItemFocused" : "bloqueioItem"} onClick={() => {setMenuActual(props.menu)}}>
+      <div className={props.menu == menuActual ? "bloqueioItem bloqueioItemFocused" : "bloqueioItem"} onClick={() => {setMenuActual(props.menu); setSubMenu(null)}}>
         <img src={props.src} />
         <span>{props.text}</span>
        </div>
     );
   }
-   function Site(props){
+  function EmptyMenu({text}){
+    return(
+      <section className="emptyMenu">
+	{ text }
+      </section>
+    );
+  }
+  function Add(props){
+    return(
+	<img className="add" src="/img/add2.svg" onClick={props.onClick && props.onClick}/>
+    );
+  }
+    
+
+ 
+  function MenuAplicativos(){
+/*    if(apps.length < 1) return <EmptyMenu text="Nenhum aplicativo adicionado" />*/
+    return(
+      <div className="menuItem" id="menuAplicativos">
+	{apps.length < 1 && <EmptyMenu text="Nenhum aplicativo adicionado" />}
+        {apps.map((elemento, index)=>{
+	  return <Aplicativo key={index} name={elemento.name} loaded={true} />
+        }
+        )}
+	<Add onClick={()=>{setSubMenu("adicionarAplicativos")}}/>
+      </div>
+    );
+  }
+
+  function Aplicativo(props){
+    const [appLoading, setAppLoading] = useState(false);
+    const [appAccepted, setAppAccepted] = useState(false);
+
+    async function addApp(nomeDaApp){
+      await sleep(1000);
+      setAppLoading(false);
+      setAppAccepted(true);
+  }
+    if(props.loaded) return(
+      <div className="app">
+         <img src="/img/android.svg"/>
+         <span>{props.name}</span>
+         <img src="/img/delete.svg"/>
+       </div>
+    );
+    else return(
+      <div className="app">
+         <img src="/img/android.svg"/>
+         <span>{props.name}</span>
+         <img src={(appLoading) ? "/img/pending.svg" : (appAccepted) ? "/img/check.svg" : "/img/add.svg"} onClick={()=>{if(!appAccepted){setAppLoading(true); addApp(props.name)}}}/>
+      </div>
+    );
+  }
+  function AdicionarAplicativos(){
+    const [searchAppLoading, setSearchAppLoading] = useState(false);
+    const [searchedApps, setSearchedApps] = useState([]);
+    async function searchApp(){
+      await sleep(1000);
+      setSearchAppLoading(false);
+      setSearchedApps([{name: "Whatsapp"}, {name: "Instagram"}])
+    }
+    return(
+      <div className="menuItem" id="adicionarAplicativos">
+	<div>
+	  <input type="text" placeholder="Insira o nome do aplicativo"/>
+	  <img src={searchAppLoading ? "/img/pending.svg" : "/img/search.svg"} onClick={()=>{setSearchAppLoading(true); searchApp()}}/>
+	</div>
+	<small>Resultados</small>
+	<section>
+	  {(searchedApps.length < 1) && <EmptyMenu text="Nenhum app encontrado"/>}
+          {searchedApps.map((elemento, index)=>{
+	    return <Aplicativo key={index} name={elemento.name} />
+          })}
+        </section>
+      </div>
+    );
+  }
+
+
+  function MenuSites(){
+    return(
+      <div className="menuItem" id="menuSites">
+        {sites.length < 1 && <EmptyMenu text="Nenhum site adicionado" /> }
+	{sites.map((elemento, index)=>{
+          return <Site key={index} domain={elemento.domain} />        }
+        )}
+	<Add />
+      </div>
+    );
+  }
+  function Site(props){
      return(
        <div className="site">
           <img src="/img/internet.svg"/>
           <span>{props.domain}</span>
-          <img src="/img/trash.svg"/>
+          <img src="/img/delete.svg"/>
         </div>
      );
-   }
-    function Aplicativo(props){
-     return(
-       <div className="app">
-          <img src="/img/android.svg"/>
-          <span>{props.name}</span>
-          <img src="/img/trash.svg"/>
-        </div>
-     );
-   }
-
+  }
   function MenuOutros(){
     return(
-      <div className="menuItem">
-	empty
+      <div className="menuItem" id="menuOutros">
+	<MenuOutrosServico img="site.svg" text="Internet" />
+	<MenuOutrosServico img="camera.svg" text="Câmera" />
+	<MenuOutrosServico img="sound.svg" text="Silencioso" />
       </div>
     );
+  }
+
+  function MenuOutrosServico(props){
+    return(
+      <div>
+        <img src={"/img/"+props.img} />
+        <span>{props.text}</span>
+        <input type="checkbox" />
+      </div>
+    )
   }
    function CloseMenu(){
      return(
@@ -50,18 +148,22 @@ export default function Bloqueios(){
        </nav >
      );
    }
+   function MainContent(){
+     	if(subMenu == "adicionarAplicativos") return <AdicionarAplicativos />
+      	if(menuActual == "aplicativos") return <MenuAplicativos />
+      	if(menuActual == "sites") return <MenuSites />
+      	if(menuActual == "outros") return <MenuOutros />
+   }
   return(
     <>
     <div id="main">
-      <div id="mainHeader">
+      <div id="mainContent">
+	<MainContent />
+      </div>
+      <div id="mainFooter">
         <BloqueioItem src="/img/android.svg" text="Aplicativos" menu="aplicativos"/>
         <BloqueioItem src="/img/internet.svg" text="Sites" menu="sites"/>
         <BloqueioItem src="/img/more.svg" text="Outros"  menu="outros"/>
-      </div>
-      <div id="mainContent">
-      	{/* menuActual == "aplicativos" && */}
-      	{/* menuActual == "sites"*/}
-      	{ menuActual == "outros" && <MenuOutros />}
       </div>
     </div>
     </>
