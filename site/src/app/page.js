d1661615ea8ecd6dcea9f691c6cdd1a749b8682c
ceutron <ceutron7@gmail.com>
2025-01-01 18:08:15 +0100

    Remodelação da página principal

--- site/src/app/page.js
+++ site/src/app/page.js
@@ -12,14 +12,14 @@ const Line = dynamic(() => import('react-chartjs-2').then((mod) => mod.Line), {
   ssr: false,
 });
 const data = {
-  labels: ['13/12', '14/12', '15/12', 'Ontem', 'Hoje'],
+  labels: ["Qui", "Sex", 'Sab', 'Dom', 'Seg', 'Ontem', 'Hoje'],
   datasets: [
     {
       label: 'Tentativas de acesso',
-      data: [65, 59, 80, 81, 56],
+      data: [65, 59, 80, 81, 56, 70, 100],
       fill: false,
       borderColor: '#55abff',
-      tension: 0.1,
+      tension: 0.3,
     },
   ],
 };
@@ -90,19 +90,7 @@ const optionsDoughnut = {
     responsive: false,
     maintainAspectRatio: false,
   };
-function MainGraphConteiner(){
-  return(
-    <div className="graphConteiner">
-	  {/*<div className="pieGraphConteiner">
-	    <Doughnut data={dataDoughnut} options={optionsDoughnut} />
-          </div>*/}
-	  {/*<div className="lineGraphConteiner">*/}
-	    <Line data={data} />
-	    <Bar data={dataBar} />
-	  {/*</div>*/}
-    </div>
-  );
-}
+
 export default function Home() {
   /*const [data, setData] = useState([
         { name: "Geeksforgeeks", students: 400 },
@@ -110,49 +98,105 @@ export default function Home() {
         { name: "Geek-i-knack", students: 200 },
         { name: "Geek-o-mania", students: 1000 },
     ]);*/
+  const [overviewMenu, setOverviewMenu] = useState("tentativas");
 
   function HeaderCard(props){
     const [mostrarAlunos, setMostrarAlunos] = useState(false);
     const [alunosSuspeitos, setAlunosSuspeitos] = useState(0);
     const [alunosModerados, setAlunosModerados] = useState(0);
     const [alunosNormais, setAlunosNormais] = useState(0);
-
+    
     return(
       <div className="headerCard" >
 	<div className="headerCardIconConteiner">
 	{props.icon}
+        {props.value >= 1000 ? props.value : props.value >= 100 ? props.value : props.value >= 10 ? "0"+props.value : "00"+props.value}
 	</div>
-        {props.value && <small>{props.value}</small>}
        	<span>{props.text}</span>
       </div>
     )
   }
+  function HomePageHeader(){
+    return(
+      <div className="homePageHeader">
+	<HeaderCard icon={<StudentsIcon color="#358bff"/>} value={8} iconBg="#358bff" text="Alunos" />
+	<HeaderCard icon={<SmartPhoneIcon color="#358bff"/>} value={12} iconBg="#358bff" text="Dispositivos" />
+	<HeaderCard icon={<AndroidIcon color="#358bff"/>} iconBg="#358bff" value={3} text={"Apps"} />
+	<HeaderCard icon={<SiteIcon color="#358bff"/>} iconBg="#358bff" value={4} text={"Sites"} />
+	{/*<HeaderCard icon={<ProfileIcon />} iconBg="#358bff" text={"ceutron"} />*/}
+      </div>
+    );
+  }
 
+  function Overview(){
+    function OverviewOption(props){
+      return(
+        <div className="overviewOption" style={props.menu == overviewMenu ? {background: "white", color: "#404040"} : {}} onClick={props.onClick && props.onClick}>
+        {props.text}
+        </div>
+      );
+    }
+
+    return(
+      <div className="overviewTit">
+	<h4>Visão geral</h4>
+        <div className="overviewOptions">
+          <OverviewOption text="Tentativas" menu="tentativas" onClick={()=>setOverviewMenu("tentativas")}/>
+          <OverviewOption text="Apps" menu="apps" onClick={()=>setOverviewMenu("apps")}/>
+          <OverviewOption text="Alunos" menu="alunos" onClick={()=>setOverviewMenu("alunos")}/>
+          <OverviewOption text="Detalhes" menu="detalhes" onClick={()=>setOverviewMenu("detalhes")}/>
+	</div>
+      </div>
+    );
+  }
+
+  function TentativasMenu(){
+    return(
+      <Line data={data}/>
+    );
+  }
+  
+  function AppsMenu(){
+    return(
+      <Bar data={dataBar} />
+    );
+  }
+
+  function AlunosMenu(){
+    return null
+  }
+  
+  function DetalhesMenu(){
+    return(
+      <Doughnut data={dataDoughnut} options={optionsDoughnut} />
+    );
+  }
+
+  function MainGraphConteiner(){
+    return(
+      <div className="homeOverview">
+        {
+	  overviewMenu == "tentativas" ?
+	  <TentativasMenu /> :
+	  overviewMenu == "apps" ?
+	  <AppsMenu /> :
+	  overviewMenu == "alunos" ?
+          <AlunosMenu /> :
+          overviewMenu == "detalhes" ?
+          <DetalhesMenu /> :
+	  null
+           
+        }
+      </div>
+    );
+  }
   return (
     <>
-    <div className="main">
-      <div id="homePageHeader">
-	<HeaderCard icon={<StudentsIcon />} value={8} iconBg="#358bff" text="alunos" />
-	<HeaderCard icon={<SmartPhoneIcon />} value={12} iconBg="#358bff" text="dispositivos" />
-	<HeaderCard icon={<AndroidIcon />} iconBg="#358bff" value={3} text={"apps"} />
-	<HeaderCard icon={<SiteIcon />} iconBg="#358bff" value={4} text={"sites"} />
-	<HeaderCard icon={<ProfileIcon />} iconBg="#358bff" text={"ceutron"} />
-      </div>
+    <div className="homePage">
+      <HomePageHeader />
+      <Overview />
       <div id="homePageContent">
 	<MainGraphConteiner />
-	<div className="alunosContent">
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-	  <Aluno nome="Zezé" dispositivos={3} ultimaConexao="há 7 dias" />
-        </div>
       </div>
     </div>
     </>
